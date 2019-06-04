e49e5281b4f516f30c0b17703786126f29ecac3b
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-06-04 08:43:09 +0300

    deleted the unnecessary requirements

--- src/model/Auth.java
+++ src/model/Auth.java
@@ -1,14 +1,9 @@
 package model;
 
-import database.DB;
-
-import java.sql.PreparedStatement;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Auth {
     List<User> users;
-    DB db;
 
     private static Auth ourInstance = new Auth(new DB());
     public static Auth getInstance() {
@@ -19,7 +14,6 @@ public class Auth {
         users.add(newUser);
     }
     private Auth(DB database) {
-        db = database;
         users = db.getUsers(); // load from JDBC
     }
 }
